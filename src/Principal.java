73287cd7b834469f8adb7ca44082e930029a0020
Leandro Costa <leandro.costa@ifba.edu.br>
2016-02-20 09:47:44 -0300

    @leandro.costa@ifba.edu.br

--- src/Principal.java
+++ src/Principal.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 
 /**
  *
- * @author Aluno
+ * @author leandro.costa@ifba.edu.br
  */
 public class Principal {
 
--- src/arvorebalanceada/ArvoreAVL.java
+++ src/arvorebalanceada/ArvoreAVL.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package arvorebalanceada;
 
 /**
  *
- * @author Aluno
+ * @author leandro.costa@ifba.edu.br
  */
 public class ArvoreAVL {
     private No raiz = null;
